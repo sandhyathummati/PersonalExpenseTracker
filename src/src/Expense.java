2cacd01f6bd864222c6a8eda37eac4eecc42baa8
sandhya thummati <sandhyathummati9@gmail.com>
2025-08-20 15:24:06 +0530

    Create Expense.java

---
+++ src/src/Expense.java
@@ -0,0 +1,33 @@
+public class Expense {
+private String category;
+private double amount;
+private String description;
+
+
+public Expense(String category, double amount, String description) {
+this.category = category;
+this.amount = amount;
+this.description = description;
+}
+
+
+public String getCategory() {
+return category;
+}
+
+
+public double getAmount() {
+return amount;
+}
+
+
+public String getDescription() {
+return description;
+}
+
+
+@Override
+public String toString() {
+return "Category: " + category + ", Amount: $" + amount + ", Description: " + description;
+}
+}
